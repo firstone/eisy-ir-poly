b5a07fa1d23f64a0686774ab9d0945a615ff3cc6
Dmitri Kostukov <firstone@erissoft.com>
2023-01-21 14:20:04 -0500

    Full reset

--- reset_flirc.sh
+++ reset_flirc.sh
@@ -5,3 +5,5 @@ chown admin:polyglot /dev/$flirc
 chmod go+rw /dev/$flirc
 
 usbconfig -d $flirc detach_kernel_driver
+usbconfig -d $flirc -i 1 detach_kernel_driver
+usbconfig -d $flirc -i 2 detach_kernel_driver
